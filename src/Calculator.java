25991b5cd3edfc528b1f99e9e8fc9f4dd9b4d7c6
Vuong3074 <mvuongk4@gmail.com>
2026-01-21 15:54:26 +0700

    Add calculator with plus operation

---
+++ src/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+
+    enum SIGN {
+        PLUS,
+        MINUS,
+        MULTIPLY,
+        DIVIDE
+    }
+
+    public Double calculate(SIGN sign, Double arg1, Double arg2) {
+
+        switch (sign) {
+
+            case PLUS:
+                return arg1 + arg2;
+
+            default:
+                break;
+        }
+
+        return 0d;
+    }
+}
